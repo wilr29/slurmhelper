075e50d34342d40e04be40f4d6a09b8587046b8f
Francisco A. C. Meyer <fcmeyer@users.noreply.github.com>
2022-03-24 16:53:47 -0300

    versioneer fix...????

--- slurmhelper/__init__.py
+++ slurmhelper/__init__.py
@@ -15,7 +15,7 @@ Submodules
 # from slurmhelper import cli, config, jobs, utils
 
 # Versioneer: add version string
-from .src._version import get_versions
+from slurmhelper.src._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
