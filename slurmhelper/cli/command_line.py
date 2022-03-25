04753af8a5546de9e906e65f3460683034eaa12b
Francisco A. C. Meyer <fcmeyer@users.noreply.github.com>
2022-03-25 13:56:45 -0300

    CLI class refactoring and reorganization

--- slurmhelper/cli/command_line.py
+++ slurmhelper/cli/command_line.py
@@ -2,7 +2,7 @@ import logging
 import os
 import shutil
 from argparse import ArgumentError
-from pprint import pprint
+import pprint
 
 import numpy as np
 import pandas as pd
@@ -34,47 +34,87 @@ class SlurmhelperCLI:
         # copy args to the object for easier access later on :)
         self.args = args
 
+        # Initialize logging
+        self.__initialize_logging()
+
+        # A manual check - need user id if using midway
+        if (
+            args.hasattr("cluster")
+            and args.hasattr("userid")
+            and args.cluster == "midway2-scratch"
+            and args.userid is None
+        ):
+            raise ArgumentError(
+                "If you are using midway2-scratch, you must provide your user ID!"
+            )
+
+        print(f"Slurmhelper will run the {args.operation} operation.")
+
+        self.logger.info("Arguments specified:")
+        self.logger.info(args)
+
+        # Load spec
+        if self.args.hasattr("spec_builtin") or self.args.hasattr("spec_file"):
+            self.__initialize_config()
+        else:
+            self.config = dict()
+            self.logger.info("Did not load any job specification.")
+
+        if self.args.hasattr("cluster") or self.args.hasattr("wd_path"):
+            self.__initialize_paths()
+
+        # Compile job list, if required
+        # below are the exceptions where one wouldn't need a job list:
+        if not (
+            (args.operation in {"list", "init"})
+            or (
+                args.operation == "gen-scripts"
+                and args.ids is None
+                and args.range is None
+            )
+            or (
+                args.operation == "check"
+                and args.hasattr("check_operation")
+                and args.check_operation in {"queue", "completion"}
+            )
+        ):
+            self.__initialize_job_list()
+
+        # run my operation! :)
+        operation = getattr(self, args.operation.replace("-", "_"))
+        operation()
+
+    def __initialize_logging(self):
         log_format = "[%(levelname)s] - %(message)s"
         # set verbosity based on arguments!
-        if args.verbose:
+        if self.args.hasattr("verbose") and self.args.verbose:
             logging.basicConfig(level=20, format=log_format)  # info
-        elif args.debug:
+        elif self.args.hasattr("debug") and self.args.debug:
             logging.basicConfig(level=10, format=log_format)  # debug
         else:
             logging.basicConfig(level=30, format=log_format)  # warning
 
         self.logger = logging.getLogger("cli")
 
-        # A manual check - need user id if using midway
-        if args.cluster == "midway2-scratch" and args.userid is None:
-            raise ArgumentError(
-                "If you are using midway2-scratch, you must provide your user ID!"
+    def __initialize_config(self):
+        if self.args.spec_file is not None:
+            self.config = load_job_spec(self.args.spec_file)
+            self.logger.info(
+                f"Loaded user-given specification from {self.args.spec_file}"
             )
-
-        print(f"Slurmhelper will run the {args.operation} operation.")
-        self.logger.info("Arguments specified:")
-        self.logger.info(args)
-
-        # Load spec
-        if args.spec_file is not None:
-            self.config = load_job_spec(args.spec_file)
-            if args.verbose:
-                self.logger.info(
-                    f"Loaded user-given specification from {args.spec_file}"
-                )
         else:
-            to_load = args.spec_builtin[0].split(":")
+            to_load = self.args.spec_builtin[0].split(":")
             spec = to_load[0]
             if len(to_load) == 1:
                 version = valid_specs[spec]["latest"]
             else:
                 version = to_load[1]
             self.config = load_builtin_spec(spec, version)
-            if args.verbose:
-                self.logger.info(
-                    f"Loaded built-in job specification: {spec} version {version}"
-                )
+            self.logger.info(
+                f"Loaded built-in job specification: {spec} version {version}"
+            )
 
+    def __initialize_paths(self):
         # Calculate directories
         if "base_directory_name" not in self.config.keys():
             self.logger.warning(
@@ -85,58 +125,46 @@ class SlurmhelperCLI:
             base_dir_name = self.config["base_directory_name"]
 
         # paths!
-        if args.cluster is not None:
-            if args.cluster[0] == "midway2-scratch":
+        if self.args.cluster is not None:
+            if self.args.cluster[0] == "midway2-scratch":
                 self.paths = calculate_directories_midwayscratch(
-                    args.userid[0], base_dir_name
+                    self.args.userid[0], base_dir_name
                 )
         else:
-            self.paths = calculate_directories(args.wd_path[0], base_dir_name)
+            self.paths = calculate_directories(self.args.wd_path[0], base_dir_name)
 
         self.logger.info("Directory tree generated:")
-        if args.verbose or args.debug:
-            pprint(self.paths)
+        self.logger.info(pprint.pformat(self.paths))
 
-        # Compile job list, if required
-        if not (
-            (args.operation in {"list", "init"})
-            or (
-                args.operation == "gen-scripts"
-                and args.ids is None
-                and args.range is None
+    def __initialize_job_list(self):
+        # load database thingy
+        self.__load_database()
+        # get valid job ids:
+        self.__valid_ids = set(self.db.order_id.values.tolist())
+
+        self.job_list = []
+        if self.args.ids is not None:
+            self.job_list += self.args.ids
+        else:
+            self.job_list += np.arange(
+                self.args.range[0], self.args.range[1] + 1
+            ).tolist()
+
+        print(
+            "A total of {n} jobs would be affected by this call.".format(
+                n=len(self.job_list)
             )
-        ):
+        )
 
-            # load database thingy
-            self.__load_database()
-            # get valid job ids:
-            self.__valid_ids = set(self.db.order_id.values.tolist())
+        self.logger.info("\nThese jobs are:")
+        self.logger.info(self.job_list)
 
-            self.job_list = []
-            if args.ids is not None:
-                self.job_list += args.ids
-            else:
-                self.job_list += np.arange(args.range[0], args.range[1] + 1).tolist()
-
-            print(
-                "A total of {n} jobs would be affected by this call.".format(
-                    n=len(self.job_list)
-                )
-            )
-
-            self.logger.info("\nThese jobs are:")
-            self.logger.info(self.job_list)
-
-            # Leverage DB to ensure job ids provided do not exceed range, or are invalid in some other way!
-            assert set(self.job_list).issubset(self.__valid_ids), (
-                f"Some job ids provided are not in the scope of "
-                f"the csv database we are using. These are: "
-                f"{set(self.job_list) - self.__valid_ids}"
-            )
-
-        # run my operation! :)
-        operation = getattr(self, args.operation.replace("-", "_"))
-        operation()
+        # Leverage DB to ensure job ids provided do not exceed range, or are invalid in some other way!
+        assert set(self.job_list).issubset(self.__valid_ids), (
+            f"Some job ids provided are not in the scope of "
+            f"the csv database we are using. These are: "
+            f"{set(self.job_list) - self.__valid_ids}"
+        )
 
     def init(self):
         initialize_directories(self.paths)
